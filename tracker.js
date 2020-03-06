fdc2d4a4591c6efe3bb6e456d428bf26ee4e8ed3
Rahul Bawa <Rahul Bawa rahul.bawa@ericsson.com>
2020-03-06 17:38:49 +0530

    Add stop feature in tracker
    When called, this would stop the tracking

--- tracker.js
+++ tracker.js
@@ -1,6 +1,8 @@
 var tracker = (function(){
     var timeSpan = 0;
     var shouldSendRequest = false;
+    var url = "";
+    var interval = -1;
     // selectors for main chat's name and status
     var selectors = {
         name: "div#main>header>div:nth-child(2)>div:nth-child(1)>div>span",
@@ -9,7 +11,7 @@ var tracker = (function(){
 
     var pollThread = function() {
         var person = getPersonInfo();
-        if (isOnline()) {
+        if (isOnline(person)) {
             console.log(person.name + " is online");
             timeSpan += 1;
             // a flag to tell if we tracked something or not
@@ -25,14 +27,18 @@ var tracker = (function(){
         }
     };
 
-    var isOnline = function() {
-        var lastSeenElement = document.querySelectorAll("._315-i._F7Vk")[0];
-        return lastSeenElement.innerHTML.indexOf("online") != -1;
+    var isOnline = function(person) {
+        return person && person.status.indexOf("online") != -1;
     };
 
+   var getNodesBy = function(css) {
+       var matches = document.querySelectorAll(css);
+       return matches.length > 0 ? matches[0].innerHTML : null;
+   };
+
    var getPersonInfo = function() {
-        var name = document.querySelectorAll(selectors.name)[0].innerHTML;
-    	var status = document.querySelectorAll(selectors.status)[0].innerHTML;
+        var name = getNodesBy(selectors.name) || '';
+    	var status = getNodesBy(selectors.status) || 'offline';
     	return {name: name, status: status};
    };
 
@@ -40,7 +46,7 @@ var tracker = (function(){
         var freq = config.freq || 1000;
         var serverUrl = config.serverUrl || 'localhost:5000';
         console.log("Initialized the polling thread");
-        window.setInterval(pollThread, freq);
+        interval = window.setInterval(pollThread, freq);
     };
 
     var sendRequest = function (data) {
@@ -56,8 +62,14 @@ var tracker = (function(){
         }).then(res=>res.json()).then(console.log);
     };
 
+    var stop = function() {
+        clearInterval(interval);
+        console.log("Tracking stopped.");
+    };
+
     return {
-        init: init
+        init: init,
+        stop: stop
     };
 })();
 tracker.init({serverUrl: 'localhost:5000', freq: 1000});
